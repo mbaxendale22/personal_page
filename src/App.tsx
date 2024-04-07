3a8442a53bd779cad00f980faabdfd9cc3a5dbac
Matthew Baxendale <mdbaxendale1@gmail.com>
2024-04-07 19:29:40 +0100

    app screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,46 @@
-import Headline from "./components/typography/Headline.tsx";
-import ScreenRoot from "./ScreenRoot.tsx";
-import Subtitle from "./components/typography/Subtitle.tsx";
-import ThemeToggleSwitch from "./components/ToggleThemeSwitch.tsx";
-import {CSSProperties} from "react";
-import TextRoot from "./components/typography/TextRoot.tsx";
+import Headline from './components/typography/Headline.tsx'
+import ScreenRoot from './ScreenRoot.tsx'
+import Subtitle from './components/typography/Subtitle.tsx'
+import ThemeToggleSwitch from './components/ToggleThemeSwitch.tsx'
+import { CSSProperties } from 'react'
+import TextRoot from './components/typography/TextRoot.tsx'
 import yllwImage2 from './assets/yllw-card-2.jpeg'
-import PreviewCard from "./components/PreviewCard.tsx";
+import PreviewCard from './components/PreviewCard.tsx'
 
 function App() {
     return (
         <ScreenRoot>
             <div style={styles.container}>
-                <div style={{alignSelf: 'flex-end'}}>
-                    <ThemeToggleSwitch/>
+                <div style={{ alignSelf: 'flex-end' }}>
+                    <ThemeToggleSwitch />
                 </div>
                 <section>
-                    <Headline content={"Hello"}/>
-                    <Subtitle content={"I'm Matthew"}/>
-                    <TextRoot content={"I'm a developer who makes cross-platform mobile apps and web apps"}/>
-                    <TextRoot content={"Here they are..."}/>
+                    <Headline content={'Hello'} />
+                    <Subtitle content={"I'm Matthew"} />
+                    <TextRoot
+                        content={
+                            "I'm a developer who makes cross-platform mobile apps and web apps"
+                        }
+                    />
+                    <TextRoot content={'Here they are...'} />
                 </section>
                 <section style={styles.cardsSection}>
-                    <PreviewCard image={yllwImage2}/>
+                    <PreviewCard image={yllwImage2} />
                 </section>
-
             </div>
-
         </ScreenRoot>
-    );
+    )
 }
 
-export default App;
+export default App
 
 const styles: Record<string, CSSProperties> = {
     container: {
-        display: "flex",
+        display: 'flex',
         flexDirection: 'column',
-        marginBottom: 36,
     },
-    cardsSection: { paddingTop: '4rem', alignSelf: 'center'},
+    cardsSection: {
+        paddingTop: '1rem',
+        alignSelf: 'center',
+    },
 }
-
-
--- src/ScreenRoot.tsx
+++ src/ScreenRoot.tsx
@@ -1,18 +1,21 @@
-import React, {CSSProperties, useContext} from 'react';
-import themeContext from "./context/ThemeContext.tsx";
+import React, { CSSProperties, useContext } from 'react'
+import themeContext from './context/ThemeContext.tsx'
 
-function ScreenRoot({children}: { children: React.ReactNode }) {
-    const {colors} = useContext(themeContext)
+function ScreenRoot({ children }: { children: React.ReactNode }) {
+    const { colors } = useContext(themeContext)
     return (
-        <div style={{...styles.container, backgroundColor: colors.mainBackground}}>
-            <div style={styles.content}>
-                {children}
-            </div>
+        <div
+            style={{
+                ...styles.container,
+                backgroundColor: colors.mainBackground,
+            }}
+        >
+            <div style={styles.content}>{children}</div>
         </div>
-    );
+    )
 }
 
-export default ScreenRoot;
+export default ScreenRoot
 
 const styles: Record<string, CSSProperties> = {
     container: {
@@ -21,11 +24,10 @@ const styles: Record<string, CSSProperties> = {
         margin: 0,
         padding: 0,
     },
-    content : {
-        margin : '0 auto',
+    content: {
+        margin: '0 auto',
         maxWidth: 1000,
         minHeight: '100%',
         padding: '2rem',
-    }
+    },
 }
-
